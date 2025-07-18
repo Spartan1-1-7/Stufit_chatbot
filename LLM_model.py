2ddccae9674af0494b6547e59c152b76a2e55526
Megha <meghatanu480@gmail.com>
2025-07-18 17:10:39 +0530

    made the RAG model in working condition using a temorary database

--- LLM_model.py
+++ LLM_model.py
@@ -13,9 +13,11 @@ load_dotenv()
 token = os.getenv("HUGGINGFACE_TOKEN")
 
 # Load FAISS index and metadata
-faiss_index = faiss.read_index("my_faiss_index.index")  # your FAISS file
-with open("docstore.pkl", "rb") as f:
+faiss_index = faiss.read_index("db_faiss/index.faiss")  # your FAISS file
+with open("db_faiss/index.pkl", "rb") as f:
     documents = pickle.load(f)  # your list of text chunks corresponding to vectors
+# print("FAISS index dimension:", faiss_index.d)
+
 
 # Load the embedding model (same one used during indexing)
 embedding_model = SentenceTransformer("all-MiniLM-L6-v2")  # or the one you used
@@ -45,13 +47,24 @@ Answer (Only use the above relevant knowledge. If not found, reply "I don't know
 # Vector search + generation
 def generate_response(user_query, top_k=3):
     query_embedding = embedding_model.encode([user_query])
+    # print("Query embedding shape:", np.array(query_embedding).shape)
     distances, indices = faiss_index.search(np.array(query_embedding), top_k)
     
-    retrieved_chunks = [documents[i] for i in indices[0] if i != -1]
-    
+    # print("Indices:", indices)
+    # print("Type:", type(indices))
+    # print("Shape:", np.shape(indices))
+
+    # retrieved_chunks = [documents[i] for i in indices[0] if i != -1]
+    retrieved_chunks = [documents[i] for i in indices[0] if i != -1 and i < len(documents)]
+
     if not retrieved_chunks:
         return "I don't know."
 
     prompt = build_prompt(user_query, retrieved_chunks)
     response = client.text_generation(prompt=prompt, max_new_tokens=300)
     return response
+
+if __name__ == "__main__":
+    query = "What is a heart-attack?"
+    answer = generate_response(query)
+    print("Generated Answer:\n", answer)
